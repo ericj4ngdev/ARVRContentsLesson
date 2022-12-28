e23faad427a449c0bd1eb78670184a4b2c927d67
ericjangdev_HomeDeskTop <ericjangdev@gmail.com>
2022-12-29 01:08:19 +0900

    VR_Begins

---
+++ VR_Begins/Assets/Door.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Vector3 targetP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetP = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetP, Time.deltaTime);
+    }
+}
